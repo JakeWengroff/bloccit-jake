806d422138c39e76bad21ee42c19419bf4bf12b0
Jake Wengroff <j@jxb1.com>
2015-04-15 00:49:16 -0400

    Associations assignment completed.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,18 @@
 class Post < ActiveRecord::Base
+  # scope :ordered_by_title, -> { order(:title, :asc) }
+  # scope :ordered_by_reverse_created_at, -> { order(:created_at, :desc) }
+
+  def self.ordered_by_title
+    order('title asc')
+  end
+
+  def self.ordered_by_reverse_created_at
+    order('created_at desc')
+  end
+
+
   has_many :comments
   belongs_to :user
 
-  default_scope { order('created_at DESC') }
+  # default_scope { order('created_at DESC') }
 end
