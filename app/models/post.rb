a1f4d937d4748567ab99fcb8f79d5e65ce84b234
Jake Wengroff <j@jxb1.com>
2015-04-17 12:44:41 -0400

    Updated Posts Index View.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,15 @@
 class Post < ActiveRecord::Base
+  def self.ordered_by_title
+    order('title asc')
+  end
+
+  def self.ordered_by_reverse_created_at
+    order('created_at desc')
+  end
+
   has_many :comments
   belongs_to :user
 
-  default_scope { order('created_at DESC') }
+  # default_scope { order('created_at DESC') }
 end
+  
--- db/migrate/20150415025025_add_user_posts.rb
+++ db/migrate/20150415025025_add_user_posts.rb
@@ -1,6 +1,6 @@
 class AddUserPosts < ActiveRecord::Migration
   def change
-    add_column :posts, :user_id, :integer
+    add_column :posts, :user_id, :integer, :name
       add_index :posts, :user_id
   end
 end
