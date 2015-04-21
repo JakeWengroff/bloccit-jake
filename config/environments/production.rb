30560652a00b0ecff04b29042cdd3762a4ac0504
Jake Wengroff <j@jxb1.com>
2015-04-20 21:09:44 -0400

    Configured app for Production emails.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'quiet-ridge-6030.herokuapp.com' }
+
+  config.force_ssl = true
+
 end
