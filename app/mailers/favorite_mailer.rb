528250ce9280178327d51c465f7a525083a5203d
Brittany Martin <brittany.jill.martin@gmail.com>
2015-05-12 22:43:52 -0700

    Fixed favoriting partial/spacing on mailer

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -1,19 +1,17 @@
-class FavoriteMailer < ActionMailer::Base
+class FavoriteMailer < ApplicationMailer
   default from: "j@jxb1.com"
 
-    def new_comment(user, post, comment)
+  def new_comment(user, post, comment)
  
-      # New Headers
-      headers["Message-ID"] = "<comments/#{comment.id}@your-app-name.example>"
-      headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
-      headers["References"] = "<post/#{post.id}@your-app-name.example>"
- 
-      @user = user
-      @post = post
-      @comment = comment
- 
-      mail(to: user.email, subject: "New comment on #{post.title}")
-    end
+    # New Headers
+    headers["Message-ID"] = "<comments/#{comment.id}@your-app-name.example>"
+    headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
+    headers["References"] = "<post/#{post.id}@your-app-name.example>"
 
+    @user = user
+    @post = post
+    @comment = comment
 
+    mail(to: user.email, subject: "New comment on #{post.title}")
+   end
 end
