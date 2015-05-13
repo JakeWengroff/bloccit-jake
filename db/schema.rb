fdd3edb1ec53aa040338b31baf2988dca87e12c1
Jake Wengroff <j@jxb1.com>
2015-05-13 16:28:37 -0400

    Checkpoint on Favoriting completed.

--- db/schema.rb
+++ db/schema.rb
@@ -33,9 +33,15 @@ ActiveRecord::Schema.define(version: 20150512155141) do
   add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "favorites", force: true do |t|
+    t.integer "post_id"
+    t.integer "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "favorites", ["post_id"], name: "index_favorites_on_post_id"
+  add_index "favorites", ["user_id"], name: "index_favorites_on_user_id"
+
 
   create_table "posts", force: true do |t|
     t.string   "title"
